85e2f43a4f078593f56a8abbc500a3ea97595a66
Adan Galicia <ansg75@gmail.com>
2023-10-02 23:07:14 -0600

    Add routers

--- inner_service/main.py
+++ inner_service/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from route.address_router import router as address_router
+from route.user_router import router as user_router
 
 app = FastAPI()
+app.include_router(router=address_router)
+app.include_router(router=user_router)
 
 
 @app.get("/")
