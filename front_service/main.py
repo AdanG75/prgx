56fc529e6690761e7751f971de47ce24b96e1b28
Adan Galicia <ansg75@gmail.com>
2023-10-02 23:50:25 -0600

    Init application's settings

--- front_service/main.py
+++ front_service/main.py
@@ -4,7 +4,7 @@ from routes.address_router import router as address_router
 from routes.health_router import route as health_router
 from routes.user_router import router as user_router
 
-app = FastAPI()
+app = FastAPI(title="PRGX-Front-Service", version="0.1.0")
 app.include_router(router=address_router)
 app.include_router(router=health_router)
 app.include_router(router=user_router)
--- inner_service/main.py
+++ inner_service/main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from route.address_router import router as address_router
 from route.user_router import router as user_router
 
-app = FastAPI()
+app = FastAPI(title="PRGX-Inner-Service", version="0.1.0")
 app.include_router(router=address_router)
 app.include_router(router=user_router)
 
 
-@app.get("/")
-async def home():
+@app.get(
+    path="/",
+    status_code=status.HTTP_200_OK,
+    tags=["Home"],
+    summary="Home of application"
+)
+async def home() -> dict:
     return {"directory": "home"}
 
